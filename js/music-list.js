8314ded24fe7e23d1ef7c396c3df356e477a3a9c
HusseinRashy10 <139256631+husseinjedi@users.noreply.github.com>
2023-07-24 08:54:19 +0100

    Commiting my music website
    Enjoy it

---
+++ js/music-list.js
@@ -0,0 +1,164 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "Save your tears",
+    artist: "The Weekend",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Hattrick",
+    artist: "Seyi vibez",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "Mara Pass Mara Beat",
+    artist: "Dj Khalipha",
+    img: "music-9",
+    src: "music-9",
+  },
+  {
+    name: "Charm",
+    artist: "Rema",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "Party no dey stop",
+    artist: "Zinoleesky, Adekunle",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "Kingdom",
+    artist: "Seyi Vibez",
+    img: "music-12",
+    src: "music-12",
+  },
+  {
+    name: "Blinding Lights",
+    artist: "The Weekend",
+    img: "music-13",
+    src: "music-13",
+  },
+  {
+    name: "Peace be upon you",
+    artist: "Asake",
+    img: "music-14",
+    src: "music-14",
+  },
+  {
+    name: "Calm down",
+    artist: "Rema",
+    img: "music-15",
+    src: "music-15",
+  },
+  {
+    name: "Ku Lo Sa",
+    artist: "Oxlade",
+    img: "music-16",
+    src: "music-16",
+  },
+  {
+    name: "Alaska",
+    artist: "Seyi Vibez",
+    img: "music-17",
+    src: "music-17",
+  },
+  {
+    name: "Unavailable",
+    artist: "Davido",
+    img: "music-18",
+    src: "music-18",
+  },
+  {
+    name: "Girlfriend",
+    artist: "Ruger",
+    img: "music-19",
+    src: "music-19",
+  },
+  {
+    name: "Peru Para",
+    artist: "Fireboy DML, Ed-sheeran",
+    img: "music-20",
+    src: "music-20",
+  },
+  {
+    name: "Playboy",
+    artist: "Fireboy DML",
+    img: "music-21",
+    src: "music-21",
+  },
+  {
+    name: "Bandana",
+    artist: "Fireboy DML, Asake",
+    img: "music-22",
+    src: "music-22",
+  },
+  {
+    name: "Gwagwalada",
+    artist: "Song by Bnxn, Kizz Daniel, and Seyi Vibez",
+    img: "music-23",
+    src: "music-23",
+  },
+  {
+    name: "Amapiano",
+    artist: "Asake, Olamide",
+    img: "music-24",
+    src: "music-24",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
